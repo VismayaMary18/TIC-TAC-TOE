e9576c723447123b63b5d6e5badc85d9de37650f
Vismaya Mary <vismayamary.mec@gmail.com>
2024-07-31 21:25:20 +0530

    Initial commit of PlacementPortal project

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (204, 153, 255) # Soft Lavender
+LINE_COLOR = (143, 63, 151) # Rich Plum
+CIRC_COLOR = (230, 190, 255) #Pastel Lavender
+CROSS_COLOR = (77, 38, 113) # Deep Violet
